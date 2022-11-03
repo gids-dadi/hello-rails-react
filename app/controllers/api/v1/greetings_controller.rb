27e0f94d7549ad3f667d481828d65a4d44ec89c5
Gideon <gideons564@gmail.com>
2022-11-03 14:38:57 +0100

    Add the weback setup for react-rails

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::GreetingsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    greetings = message.find_by(id: rand(1..5))
+    if messages
+      render json: messages
+    else
+      render json:messages.errors, status: :bad_request
+    end
+  end
+end
